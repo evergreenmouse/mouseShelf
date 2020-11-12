a5db681380e8d702a168340d0ac5e4516cd1e260
evergreenmouse <evergreenmouse@gmail.com>
2020-11-12 16:23:03 +0300

    Initial Commit

---
+++ mouseShelf/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  mouseShelf
+//
+//  Created by Александр Ополовников on 12.11.2020.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ mouseShelf/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  mouseShelf
+//
+//  Created by Александр Ополовников on 12.11.2020.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
